e1b7e2a12199e7a250089be592cd7569d785d8a4
Akshay Chavan <chavanay@users.noreply.github.com>
2015-11-03 15:51:54 -0500

    Create func1.js

---
+++ func1.js
@@ -0,0 +1,18 @@
+<html>
+<head> Function without parameter</head>
+
+<script type="text/javascript">
+
+function sayHello( name, age)
+{
+ document.write("hey there, my name  is" +name +" and age is "+ age + "years old");
+}
+</head>
+
+<body>
+<p> Function 1:</p>
+<form>
+<input type="button" onClick="sayHello('Aki', 26)" value="say Hello">
+</form>
+</body>
+</html>
